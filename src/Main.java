d692954591ae4eba94dc6189546a272a981b3be8
Piotr Wrobel <opticpiter@gmail.com>
2021-01-11 15:25:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         ParcelLocker locker1 = new ParcelLocker();
-        locker1.addParcel(new ParcelA("ABC-12345", 12));
-        locker1.addParcel(new ParcelA("ADC-44215", 123));
-        locker1.addParcel(new ParcelB("AAD-44112", 125));
+        locker1.addParcel(new ParcelB("AAA-12124", 44));
+        locker1.addParcel(new ParcelB("AAB-12124", 124));
+        locker1.addParcel(new ParcelA("DAA-12124", 44));
 
-        locker1.getParcels();
-        locker1.removeAllParcels();
-        locker1.getParcels();
+        locker1.listParcels();
+        locker1.removeParcel("AAA-12124");
+        locker1.listParcels();
     }
 }
